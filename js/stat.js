a5abceeb0267d78e39ccf111658193cf7ef12a68
Kindro5654 <aleks.bochar@gmail.com>
2020-09-30 23:56:42 +0300

    fixed issues with ESLint

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-var */
 "use strict";
 
 var CLOUD_WIDTH = 420;
@@ -18,7 +19,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+  let maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -30,39 +31,39 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.7)");
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
 
-  ctx.fillStyle = "#000";
-  ctx.font = "16px PT Mono";
-  ctx.fillText("Ура вы победили!", 125, 40);
-  ctx.fillText("Список результатов:", 125, 57);
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.fillText(`Ура вы победили!`, 125, 40);
+  ctx.fillText(`Список результатов:`, 125, 57);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = `#000`;
     ctx.fillText(
-      players[i],
-      CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
-      CLOUD_Y + GAP + TEXT_Y
+        players[i],
+        CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
+        CLOUD_Y + GAP + TEXT_Y
     );
     ctx.fillText(
-      Math.round(times[i]),
-      CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
-      CLOUD_Y + COL_Y - (barWidth * times[i]) / maxTime
+        Math.round(times[i]),
+        CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
+        CLOUD_Y + COL_Y - (barWidth * times[i]) / maxTime
     );
-    if (players[i] == "Вы") {
-      ctx.fillStyle = "#e30e0e";
+    if (players[i] == `Вы`) {
+      ctx.fillStyle = `#e30e0e`;
     } else {
       ctx.fillStyle =
         `hsl(245, 84%, ${Math.floor((Math.random() * (90 - 20)) + 20)}%)`;
     }
     ctx.fillRect(
-      CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
-      CLOUD_Y + GAP + COL_Y,
-      COL_WIDTH,
-      -(barWidth * times[i]) / maxTime
+        CLOUD_X + GAP + FONT_GAP + (COL_WIDTH + COL_DIST) * i,
+        CLOUD_Y + GAP + COL_Y,
+        COL_WIDTH,
+        -(barWidth * times[i]) / maxTime
     );
   }
 };
